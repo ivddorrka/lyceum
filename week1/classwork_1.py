8ce093a9d8049bd8314b181f1738e7637436e6b4
ivddorrka <ivddorrka@gmail.com>
2021-02-06 18:05:06 +0200

    Done in class

---
+++ week1/classwork_1.py
@@ -0,0 +1,13 @@
+# print("Hello, world")
+number_1 = 5
+
+number_2 = 0
+# ПОЯСНЕННЯ
+# print(a + b)
+
+
+yyyyyyyyyy = number_1 / number_1
+print(yyyyyyyyyy)
+
+yyyyyyyyyy = number_1 * number_2
+print(yyyyyyyyyy)
